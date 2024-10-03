cc1722b6e1de272033da647a9a2cefe18065b5a0
Manish Thulung <manish.thulung58@gmail.com>
2024-10-04 01:54:56 +0545

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex gap-8">
-      <Link href={"/post"}>
-        <Button variant={"link"}>Posts</Button>
-      </Link>
-      <Link href={"/photo"}>
-        <Button variant={"link"}>Photos</Button>
-      </Link>
+    <div className="flex justify-center items-center flex-col gap-10 mt-10">
+      <h1 className="text-3xl text-red-600 underline">
+        This is the react table Demo/Article
+      </h1>
+      <div className="flex gap-8">
+        <Link href={"/post"}>
+          <Button variant={"destructive"} className="text-xl font-semibold">Posts</Button>
+        </Link>
+        <Link href={"/photo"}>
+          <Button variant={"destructive"} className="text-xl font-semibold">Photos</Button>
+        </Link>
+      </div>
     </div>
   );
 }
